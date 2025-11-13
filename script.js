bfb2b36573a510307096fc7ea64e5e6471f07ea1
dunnoknow2 <109828581+dunnoknow2@users.noreply.github.com>
2025-11-13 21:59:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Wait for the document to load
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickMeBtn");
+  button.addEventListener("click", () => {
+    alert("Nothing to find here!");
+  });
+});
